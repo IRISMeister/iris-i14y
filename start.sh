f0fc6280dfec31c2ab098231c5fd06eb18557fad
IRISMeister <12710874+IRISMeister@users.noreply.github.com>
2023-05-17 18:16:44 +0900

    add activity monitor

--- start.sh
+++ start.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-docker-compose up -d
+docker compose up -d postgres sftp ftp smtp iris2 
+docker compose exec -T iris2 bash -c "\$ISC_PACKAGE_INSTALLDIR/dev/Cloud/ICM/waitISC.sh '' 60"
+# wait until msgbank production is ready
+sleep 5
+docker compose up -d iris
 
 echo "SMP | http://localhost:52773/csp/sys/%25CSP.Portal.Home.zen"
